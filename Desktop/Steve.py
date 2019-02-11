8d2daf4b5a73c5a5ad7da09398994f630eff0a4a
lerker100 <lerker100@protonmail.com>
2019-02-11 09:08:06 -0500

    rewriting the Steve.py file

--- Desktop/Steve.py
+++ Desktop/Steve.py
@@ -17,12 +17,26 @@ from modules import greating
 from modules import DateTime
 from modules import wordtonum
 
+# Veriables
+IsFound = False
+
 Speek.Speek("Hi I'm Steve")
 
-# defining the keyword to trigger steve
+# defining the keyphrase to trigger steve
 speech = LiveSpeech(lm=False, keyphrase='steve', kws_threshold=1e-20)
 
-# an infinate loop that checks every word
+# an infinate loop that checks every word to see if it matches the keyphrase
 for phrase in speech:
   words = phrase.segments(detailed=False)
-  print(words)
+  if 'steve' in words:
+    print('active')
+    
+    # starts a realtime speech to text seshion
+    for text in LiveSpeech():
+      if IsFound == False:
+        print(str(text))
+        IsFound = True
+        
+      else:
+        IsFound = False
+        break
