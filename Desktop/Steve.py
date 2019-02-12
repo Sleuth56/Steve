a8cab2bc2269769b7ea947d809c2da50c278fe15
lerker100 <lerker100@protonmail.com>
2019-02-12 13:20:58 -0500

    fixing some bugs in the desktop verson

--- Desktop/Steve.py
+++ Desktop/Steve.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-# Must have librarries
-import time
-import os
-# steve specific external imports
-from pocketsphinx import LiveSpeech
-import pyttsx3
-import _thread
-# steve modules
+#Steve module imports
 from modules import Speek
 from modules import web
 from modules import comp
@@ -17,26 +10,69 @@ from modules import greating
 from modules import DateTime
 from modules import wordtonum
 
-# Veriables
-IsFound = False
+# Must have librarries
+import time
+import os
+
+# other inports
+from pocketsphinx import LiveSpeech
+import pyttsx3
+import _thread
+import time
 
 Speek.Speek("Hi I'm Steve")
 
-# defining the keyphrase to trigger steve
-speech = LiveSpeech(lm=False, keyphrase='steve', kws_threshold=1e-20)
-
-# an infinate loop that checks every word to see if it matches the keyphrase
-for phrase in speech:
-  words = phrase.segments(detailed=False)
+for words in LiveSpeech():
   if 'steve' in words:
-    print('active')
-    
-    # starts a realtime speech to text seshion
-    for text in LiveSpeech():
-      if IsFound == False:
-        print(str(text))
-        IsFound = True
-        
-      else:
-        IsFound = False
-        break
+    words = wordtonum.wordtonum(str(words))
+    words = str(words)
+    words = words.replace(' ', '_')
+    print(words)
+
+    # if found == False:
+    #   for i in range(len(your_module.quorry())):
+    #     if your_module.quorry()[i] in words:
+    #       found = True
+    #       your_module.Steve(words)
+
+    if found == False:
+      for i in range(len(DateTime.quorry())):
+        if DateTime.quorry()[i] in words:
+          found = True
+          DateTime.Steve(words)
+
+    if found == False:
+      for i in range(len(web.quorry())):
+        if web.quorry()[i] in words:
+          found = True
+          web.Steve(words)
+
+    if found == False:
+      for i in range(len(comp.quorry())):
+        if comp.quorry()[i] in words:
+          found = True
+          comp.Steve(words)
+
+    if found == False:
+      for i in range(len(Wikipedia.quorry())):
+        if Wikipedia.quorry()[i] in words:
+          found = True
+          Wikipedia.Steve(words)
+
+    if found == False:
+      for i in range(len(Timer.quorry())):
+        if Timer.quorry()[i] in words:
+          found = True
+          Timer.Steve(words)
+
+    if found == False:
+      for i in range(len(greating.quorry())):
+        if greating.quorry()[i] in words:
+          found = True
+          greating.Steve(words)
+
+    if found == False:
+      for i in range(len(Speek.quorry())):
+        if Speek.quorry()[i] in words:
+          found = True
+          Speek.Steve(words)
